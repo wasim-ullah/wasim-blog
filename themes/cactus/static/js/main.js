1a5dca65e320a1c9e654cd711dc8b6d42651ef8e
Wasim Ullah <mrwullah@gmail.com>
2018-11-26 22:44:30 +0400

    Update main.js

--- themes/cactus/static/js/main.js
+++ themes/cactus/static/js/main.js
@@ -34,3 +34,12 @@ function retina() {
 };
  
 $(document).ready(retina);
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
