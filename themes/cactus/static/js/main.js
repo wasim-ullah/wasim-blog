a28b6f2cecad053c8c8232b4553ddb1ca20c65b4
Wasim Ullah <wasim.ullah@aol.com>
2019-09-30 23:42:00 +0400

    Update main.js

--- themes/cactus/static/js/main.js
+++ themes/cactus/static/js/main.js
@@ -72,3 +72,29 @@ $(document).ready(function() {
 	  });
 	});
 	});
+
+
+
+
+
+$('#toggle-box-checkbox').on('change', function(){
+  if(this.checked){
+    $('body').addClass('night');
+  }else{
+    $('body').removeClass('night');
+  }
+});
+
+
+
+
+
+// run demo if in searchresult preview
+function demo(){
+    setInterval(function(){
+      $("#toggle-box-checkbox").trigger('click');
+    }, 1000);
+}
+if (document.location.pathname.indexOf('fullcpgrid')>-1){
+  demo();
+}
