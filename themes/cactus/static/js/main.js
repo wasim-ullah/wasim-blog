034cd32b323ed1a91e0b632d041364bd6ac514f4
Rehmat karim <rehmat.karim.khan@gmail.com>
2020-07-06 02:47:19 +0500

    Update main.js

--- themes/cactus/static/js/main.js
+++ themes/cactus/static/js/main.js
@@ -1,5 +1,60 @@
 // To make images retina, add a class "2x" to the img element
 // and add a <image-name>@2x.png image. Assumes jquery is loaded.
+
+
+var style_cookie_name = "style" ;
+var style_cookie_duration = 30 ;
+
+function switch_style ( css_title )
+{
+  var i, link_tag ;
+  for (i = 0, link_tag = document.getElementsByTagName("link") ;
+    i < link_tag.length ; i++ ) {
+    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+      link_tag[i].title) {
+      link_tag[i].disabled = true ;
+      if (link_tag[i].title == css_title) {
+        link_tag[i].disabled = false ;
+      }
+    }
+    set_cookie( style_cookie_name, css_title,
+      style_cookie_duration, style_domain );
+  }
+}
+function set_style_from_cookie()
+{
+  var css_title = get_cookie( style_cookie_name );
+  if (css_title.length) {
+    switch_style( css_title );
+  }
+}
+function set_cookie ( cookie_name, cookie_value,
+    lifespan_in_days, valid_domain )
+{
+
+    var domain_string = valid_domain ?
+                       ("; domain=" + valid_domain) : '' ;
+    document.cookie = cookie_name +
+                       "=" + encodeURIComponent( cookie_value ) +
+                       "; max-age=" + 60 * 60 *
+                       24 * lifespan_in_days +
+                       "; path=/" + domain_string ;
+}
+function get_cookie ( cookie_name )
+{
+	var cookie_string = document.cookie ;
+	if (cookie_string.length != 0) {
+		var cookie_array = cookie_string.split( '; ' );
+		for (i = 0 ; i < cookie_array.length ; i++) {
+			cookie_value = cookie_array[i].match ( cookie_name + '=(.*)' );
+			if (cookie_value != null) {
+				return decodeURIComponent ( cookie_value[1] ) ;
+			}
+		}
+	}
+	return '' ;
+}
+//****************************************************************************************************************
  
 function isRetina() {
 	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
@@ -16,30 +71,6 @@ function isRetina() {
 	return false;
 };
 
-/* Button Actions */
-function buttonAction() {
-  // Get the checkbox
-  var checkBox = document.getElementById("check")
-  // If the checkbox is checked, display the output text
-  if (checkBox.checked == true){
-   changeTheme("original.css");
-  } else {
-   changeTheme("style.css");
-  }
-}
-
-/* These lines will change the themese */
-
-function changeTheme(original) {
- if (document.styleSheets) {
-  var nStyles = document.styleSheets.length;
-  for (var i=0;i<nStyles;i++) {
-   if (i==original) document.styleSheets[i].disabled = false;
-   else      document.styleSheets[i].disabled = true;
-  }
- }
-}
- 
 function retina() {
 	
 	if (!isRetina())
